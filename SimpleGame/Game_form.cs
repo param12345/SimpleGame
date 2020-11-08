5de882c11833bba7c148ebf8016a1f305b3cfcdf
Paramjeet Kaur <parampanjeta3@gmail.com>
2020-11-09 11:00:55 +1300

    Form rename

--- SimpleGame/Game_form.cs
+++ SimpleGame/Game_form.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SimpleGame
 {
-    public partial class Form1 : Form
+    public partial class Game_form : Form
     {
-        public Form1()
+        public Game_form()
         {
             InitializeComponent();
         }
