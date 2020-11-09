650cb75c322c9918f78ad016dc2b5e5fbab371e5
Paramjeet Kaur <parampanjeta3@gmail.com>
2020-11-09 14:59:04 +1300

    comments

--- SimpleGame/Game_form.Designer.cs
+++ SimpleGame/Game_form.Designer.cs
@@ -28,33 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.btn_spin = new System.Windows.Forms.Button();
+            this.Btn_shoot = new System.Windows.Forms.Button();
+            this.PicBox_Main = new System.Windows.Forms.PictureBox();
+            this.Btn_Exit = new System.Windows.Forms.Button();
+            this.Btn_PlayAgain = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox_Main)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btn_load
             // 
-            this.button1.Location = new System.Drawing.Point(156, 82);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_load.Location = new System.Drawing.Point(88, 324);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(75, 49);
+            this.btn_load.TabIndex = 0;
+            this.btn_load.Text = "Load";
+            this.btn_load.UseVisualStyleBackColor = true;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
             // 
-            // Form1
+            // btn_spin
+            // 
+            this.btn_spin.Location = new System.Drawing.Point(316, 324);
+            this.btn_spin.Name = "btn_spin";
+            this.btn_spin.Size = new System.Drawing.Size(75, 49);
+            this.btn_spin.TabIndex = 1;
+            this.btn_spin.Text = "Spin";
+            this.btn_spin.UseVisualStyleBackColor = true;
+            this.btn_spin.Click += new System.EventHandler(this.btn_spin_Click);
+            // 
+            // Btn_shoot
+            // 
+            this.Btn_shoot.Location = new System.Drawing.Point(500, 324);
+            this.Btn_shoot.Name = "Btn_shoot";
+            this.Btn_shoot.Size = new System.Drawing.Size(75, 49);
+            this.Btn_shoot.TabIndex = 2;
+            this.Btn_shoot.Text = "Shoot";
+            this.Btn_shoot.UseVisualStyleBackColor = true;
+            // 
+            // PicBox_Main
+            // 
+            this.PicBox_Main.Location = new System.Drawing.Point(180, 63);
+            this.PicBox_Main.Name = "PicBox_Main";
+            this.PicBox_Main.Size = new System.Drawing.Size(386, 200);
+            this.PicBox_Main.TabIndex = 3;
+            this.PicBox_Main.TabStop = false;
+            // 
+            // Btn_Exit
+            // 
+            this.Btn_Exit.Location = new System.Drawing.Point(640, 85);
+            this.Btn_Exit.Name = "Btn_Exit";
+            this.Btn_Exit.Size = new System.Drawing.Size(75, 49);
+            this.Btn_Exit.TabIndex = 4;
+            this.Btn_Exit.Text = "Exit";
+            this.Btn_Exit.UseVisualStyleBackColor = true;
+            // 
+            // Btn_PlayAgain
+            // 
+            this.Btn_PlayAgain.Location = new System.Drawing.Point(640, 195);
+            this.Btn_PlayAgain.Name = "Btn_PlayAgain";
+            this.Btn_PlayAgain.Size = new System.Drawing.Size(75, 49);
+            this.Btn_PlayAgain.TabIndex = 5;
+            this.Btn_PlayAgain.Text = "Play Again";
+            this.Btn_PlayAgain.UseVisualStyleBackColor = true;
+            this.Btn_PlayAgain.Click += new System.EventHandler(this.Btn_PlayAgain_Click);
+            // 
+            // Game_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.Btn_PlayAgain);
+            this.Controls.Add(this.Btn_Exit);
+            this.Controls.Add(this.PicBox_Main);
+            this.Controls.Add(this.Btn_shoot);
+            this.Controls.Add(this.btn_spin);
+            this.Controls.Add(this.btn_load);
+            this.Name = "Game_form";
+            this.Text = "Game Form";
+            this.Load += new System.EventHandler(this.Game_form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox_Main)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.Button btn_spin;
+        private System.Windows.Forms.Button Btn_shoot;
+        private System.Windows.Forms.PictureBox PicBox_Main;
+        private System.Windows.Forms.Button Btn_Exit;
+        private System.Windows.Forms.Button Btn_PlayAgain;
     }
 }
 
--- SimpleGame/Game_form.cs
+++ SimpleGame/Game_form.cs
@@ -16,5 +16,31 @@ namespace SimpleGame
         {
             InitializeComponent();
         }
+
+        // Game_form_Load function will disable all the buttons except Load button
+        private void Game_form_Load(object sender, EventArgs e)
+        {
+            btn_spin.Enabled = false; //disabling the Spin button
+            Btn_shoot.Enabled = false; 
+        }
+
+        private void btn_load_Click(object sender, EventArgs e)
+        {
+            btn_spin.Enabled = true;
+            btn_load.Enabled = false;
+        }
+
+        private void btn_spin_Click(object sender, EventArgs e)
+        {
+            Btn_shoot.Enabled = true;
+            btn_spin.Enabled = false;
+        }
+
+        private void Btn_PlayAgain_Click(object sender, EventArgs e)
+        {
+            btn_load.Enabled = true;
+            btn_spin.Enabled = false;
+            Btn_shoot.Enabled = false;
+        }
     }
 }
