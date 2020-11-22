8767cca3368c4bafa1b7a6e2584158c97ffdad25
Paramjeet Kaur <parampanjeta3@gmail.com>
2020-11-23 11:36:10 +1300

    unit testing

---
+++ SimpleGameUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleGameUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleGameUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("04266f13-3c96-4c51-93a4-da3efc8b364b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SimpleGameUnitTestProject/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleGame;
+
+namespace SimpleGameUnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Game_logic unitTesting_obj = new Game_logic();
+
+        [TestMethod]
+        public void Shoot_Test()
+        {
+            unitTesting_obj.load_data = 1;
+            unitTesting_obj.spin_data = 1;
+            int testResult = unitTesting_obj.shoot_method();
+            Assert.AreEqual(1, testResult);
+        }
+        [TestMethod]
+        public void Shoot_Test_Neg()
+        {
+            unitTesting_obj.load_data = 1;
+            unitTesting_obj.spin_data = 5;
+            int testResult = unitTesting_obj.shoot_method();
+            Assert.AreNotEqual(1, testResult);
+        }
+    }
+}
